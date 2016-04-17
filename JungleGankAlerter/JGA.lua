b0d0a69c2004081b59713669f21fd4ca6e816b01
RequiredGoS <desafiatumenteyt@gmail.com>
2016-04-17 10:59:14 +0200

    Update JGA.lua

--- JungleGankAlerter/JGA.lua
+++ JungleGankAlerter/JGA.lua
@@ -1,3 +1,19 @@
+local ver = "0.01"
+
+function AutoUpdate(data)
+    if tonumber(data) > tonumber(ver) then
+        PrintChat("New AnnieGod Script Version Found " .. data)
+        PrintChat("Downloading update, please wait...")
+        DownloadFileAsync("https://raw.githubusercontent.com/RequiredGoS/Gaming-On-Steroids/master/JungleGankAlerter/JGA.lua", SCRIPT_PATH .. "JungleGankAlerter.lua", function() PrintChat(string.format("<font color=\"#FC5743\"><b>Script Downloaded succesfully. please 2x f6</b></font>")) return end)
+    else
+        PrintChat(string.format("<font color=\"#fa87b4\"><b>Script Up to date.</b></font>"))
+    end
+end
+
+GetWebResultAsync("https://raw.githubusercontent.com/RequiredGoS/Gaming-On-Steroids/master/JungleGankAlerter/JGA.version", AutoUpdate)
+
+
+
 local JGA = MenuConfig("JGA", "JungleGankAlerter")
 
 JGA:Menu("jungleGA", "Jungle Gank Alerter")
@@ -5,6 +21,11 @@ JGA.jungleGA:Boolean("Enabled", "Enable Jungle Gank Alerter", true)
 JGA.jungleGA:Boolean("Sc", "Silent Check?", false)
 JGA.jungleGA:Info("I1", "Silent Check = No drawing")
 JGA.jungleGA:Slider("jCh", "Range to check jungler", 2690, 300, 7000)
+
+JGA:Menu("Inf", "Information")
+JGA.Inf:Info("I2", "Current Version ... 0.01")
+JGA.Inf:Info("I3", "Made By Required")
+JGA.Inf:Info("I4", "Upove if you liked it")
 
 
 local GotSmite = nil
