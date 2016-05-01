2ce33aefaf113b4d8bf32e46a9df514e85db00da
RequiredGoS <desafiatumenteyt@gmail.com>
2016-05-01 11:43:09 +0200

    Update Required_Utilities.lua
    Added Model Changer

--- Required-Utilities/Required_Utilities.lua
+++ Required-Utilities/Required_Utilities.lua
@@ -1,7 +1,7 @@
 require 'DamageLib'
 
 
-local ver = "1.07 "
+local ver = "1.08 "
 
 local res = GetResolution()
 
@@ -106,6 +106,9 @@ Required.Req:SubMenu("SubReq3", "Skin Changer")
                                     Required.Req.SubReq3:Boolean("SkinEnabled", "Enable Skin Changer", true)
                                     Required.Req.SubReq3:Empty("Empty3", 0)
                                     Required.Req.SubReq3:Slider("SkinID", "Select Skin", 1, 1, 10)
+                                    Required.Req.SubReq3:Boolean("ModelChanger", "Enable Model Changer", false)
+                                    Required.Req.SubReq3:DropDown("ModelID", "Select Model", 1, {"Baron", "Dragon", "RiftHerald", "Chaos Minion", "Order Minion"})
+
 end
 if ReqLoader.Load.LoadSkin:Value() then   
     SkinChangerMenu()
@@ -594,38 +597,43 @@ end
 end
 end)
 
-
+newUpdate = 0
 function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         PrintChat("New Version Found " .. data)
         PrintChat("Downloading update, please wait...")
-        OnDraw(function()
-   local w, h1, h2, size = (res.x*0.70), (res.y*.15), (res.y*.9), res.y*.02
-      DrawLine(w, h1/1.05, w, h2/1.97, w/1.75, ARGB(120,205,0,0))
-      DrawLine(w, h1, w, h2/1.97, w/1.75, ARGB(120,50,0,0))
-      DrawText(tostring("Required Utilities Changelog"), (res.y * .028) - 270, (res.x/2.4) - 30, (res.y*.18-10), ARGB(255, 0 , 255, 255))
-      DrawText(tostring("Ver 1.06:"), res.y*.015, (res.x/2.65), (res.y*.210), ARGB(225, 225, 175, 0))
-      DrawText(tostring("               Added Changelog Screen"), res.y*.015, (res.x/2.65), (res.y*.225), ARGB(255, 255, 255, 255))
-      DrawText(tostring("Ver 1.05"), res.y*.015, (res.x/2.65), (res.y*.240), ARGB(225, 225, 175, 0))
-      DrawText(tostring("               Fixed Auto Level, Auto Smite."), res.y*.015, (res.x/2.65), (res.y*.255), ARGB(255, 255, 255, 255))
-      DrawText(tostring(""), res.y*.015, (res.x/2.65), (res.y*.270), ARGB(255, 255, 255, 255))
-      DrawText(tostring(""), res.y*.015, (res.x/2.65), (res.y*.300), ARGB(255, 255, 255, 255))
-      DrawText(tostring("TODO:"), res.y*.015, (res.x/2.65), (res.y*.315), ARGB(225, 225, 175, 0))
-      DrawText(tostring("             Add Model Changes."), res.y*.015, (res.x/2.65), (res.y*.330), ARGB(255, 255, 255, 255))
-      DrawText(tostring("             Add Rest of the objects."), res.y*.015, (res.x/2.65), (res.y*.345), ARGB(255, 255, 255, 255))
-      DrawText(tostring("             Particles."), res.y*.015, (res.x/2.65), (res.y*.360), ARGB(255, 255, 255, 255))
-      DrawText(tostring("             Suggestions?"), res.y*.015, (res.x/2.65), (res.y*.375), ARGB(255, 255, 255, 255))
-      local w1, w2, h1, h2 = (res.x/2)-50, (res.x/2)+50, (res.y*.70), (res.y*.75)
-      DrawLine(w1, h1/1.775, w2, h1/1.775, 50, ARGB(122, 253, 55, 121)) 
-      --DrawLine(w*.98, h1*.98, w*.98, h2*.98, w*.1*.98, ARGB(205,255,255,255))
-      FillRect(w1+10, (res.y/2)-101, 80, 30, ARGB(255,125,155,225))
-      DrawText(tostring("2x F6"),size, (res.x/2-12)-size+10, (res.y/2)-100, ARGB(255,0, 0, 0))
-      end)
+        newUpdate = 1
         DownloadFileAsync("https://raw.githubusercontent.com/RequiredGoS/Gaming-On-Steroids/master/Required-Utilities/Required_Utilities.lua", SCRIPT_PATH .. "Required_Utilities.lua", function() PrintChat(string.format("<font color=\"#FC5743\"><b>Script Downloaded succesfully. please 2x f6</b></font>")) return end)
     end
 end
 GetWebResultAsync("https://raw.githubusercontent.com/RequiredGoS/Gaming-On-Steroids/master/Required-Utilities/Required_Utilities.version", AutoUpdate)
 
+if newUpdate == 1 then
+    OnDraw(function()
+
+       local w, h1, h2, size = (res.x*0.70), (res.y*.15), (res.y*.9), res.y*.02
+          DrawLine(w, h1/1.05, w, h2/1.97, w/1.75, ARGB(120,205,0,0))
+          DrawLine(w, h1, w, h2/1.97, w/1.75, ARGB(120,50,0,0))
+          DrawText(tostring("Required Utilities Changelog"), (res.y * .028) - 270, (res.x/2.4) - 30, (res.y*.18-10), ARGB(255, 0 , 255, 255))
+          DrawText(tostring("Ver 1.08:"), res.y*.015, (res.x/2.65), (res.y*.210), ARGB(225, 225, 175, 0))
+          DrawText(tostring("               Added Model Changer"), res.y*.015, (res.x/2.65), (res.y*.225), ARGB(255, 255, 255, 255))
+          DrawText(tostring("Ver 1.07"), res.y*.015, (res.x/2.65), (res.y*.240), ARGB(225, 225, 175, 0))
+          DrawText(tostring("               Added Changelog Screen."), res.y*.015, (res.x/2.65), (res.y*.255), ARGB(255, 255, 255, 255))
+          DrawText(tostring(""), res.y*.015, (res.x/2.65), (res.y*.270), ARGB(255, 255, 255, 255))
+          DrawText(tostring(""), res.y*.015, (res.x/2.65), (res.y*.300), ARGB(255, 255, 255, 255))
+          DrawText(tostring("TODO:"), res.y*.015, (res.x/2.65), (res.y*.315), ARGB(225, 225, 175, 0))
+          DrawText(tostring("             Add Model Changes. DONE"), res.y*.015, (res.x/2.65), (res.y*.330), ARGB(255, 255, 255, 255))
+          DrawText(tostring("             Add Rest of the objects."), res.y*.015, (res.x/2.65), (res.y*.345), ARGB(255, 255, 255, 255))
+          DrawText(tostring("             Particles."), res.y*.015, (res.x/2.65), (res.y*.360), ARGB(255, 255, 255, 255))
+          DrawText(tostring("             Suggestions?"), res.y*.015, (res.x/2.65), (res.y*.375), ARGB(255, 255, 255, 255))
+          local w1, w2, h1, h2 = (res.x/2)-50, (res.x/2)+50, (res.y*.70), (res.y*.75)
+          DrawLine(w1, h1/1.775, w2, h1/1.775, 50, ARGB(122, 253, 55, 121)) 
+          --DrawLine(w*.98, h1*.98, w*.98, h2*.98, w*.1*.98, ARGB(205,255,255,255))
+          FillRect(w1+10, (res.y/2)-101, 80, 30, ARGB(255,125,155,225))
+          DrawText(tostring("2x F6"),size, (res.x/2-12)-size+10, (res.y/2)-100, ARGB(255,0, 0, 0))
+          newUpdate = 0
+    end)
+end
 
 OnTick(function(myHero)
 
@@ -693,16 +701,26 @@ end
             end 
         end
 
-if ReqLoader.Load.LoadSkin:Value() then 
-        if Required.Req.SubReq3.SkinEnabled:Value() ~= false then
-            HeroSkinChanger(myHero, Required.Req.SubReq3.SkinID:Value() - 1)
-        end
-end
-
-        
-
-
+    if ReqLoader.Load.LoadSkin:Value() then 
+            if Required.Req.SubReq3.SkinEnabled:Value() ~= false then
+                HeroSkinChanger(myHero, Required.Req.SubReq3.SkinID:Value() - 1)
+                if Required.Req.SubReq3.ModelChanger:Value() then
+                    if Required.Req.SubReq3.ModelID:Value() == 1 then
+                        ModelChanger(myHero, 0, "SRU_Baron")
+                    elseif Required.Req.SubReq3.ModelID:Value() == 2 then
+                        ModelChanger(myHero, 0, "SRU_Dragon")
+                    elseif Required.Req.SubReq3.ModelID:Value() == 3 then
+                        ModelChanger(myHero, 0, "SRU_RiftHerald")
+                    elseif Required.Req.SubReq3.ModelID:Value() == 4 then
+                        ModelChanger(myHero, 0, "SRU_ChaosMinionSiege")
+                    elseif Required.Req.SubReq3.ModelID:Value() == 5 then
+                        ModelChanger(myHero, 0, "SRU_OrderMinionSiege")
+                    end
+                else ModelChanger(myHero, 0, GetObjectModelName(myHero))
+                end
+            end
     end
+end
 end)
 
 OnDeleteObj(function(object)
