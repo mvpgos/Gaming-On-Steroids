21dcb1923e08f00f1c75d4dfacc3139618940573
RequiredGoS <desafiatumenteyt@gmail.com>
2016-05-01 17:08:52 +0200

    Create MinionHP.lua

---
+++ MinionHP/MinionHP.lua
@@ -0,0 +1,47 @@
+local version = "0.1"
+local HpMenu = MenuConfig("Minion HP: Version: "..version.."", "Requireds Minion HP Drawing")
+
+HpMenu:SubMenu("sel", "Drawings")
+	HpMenu.sel:Boolean("Enabled", "Enable HP Drawings", true)
+		HpMenu.sel:SubMenu("select", "Select Minions")
+			HpMenu.sel.select:Boolean("mcr", "Draw on Ranged Minions", true)
+			HpMenu.sel.select:Boolean("mcm", "Draw on Melee Minions", true)
+			HpMenu.sel.select:Boolean("mcs", "Draw on Siege Minions", true)
+
+OnDraw(function()
+	if HpMenu.sel.Enabled:Value() then
+		for i,minion in pairs(minionManager.objects) do
+			if GetTeam(minion) ~= myHero.team then
+				if HpMenu.sel.select.mcr:Value() then
+					if GetObjectName(minion) == "SRU_ChaosMinionRanged" then
+						mcr = minion
+						DrawText(""..math.ceil(GetCurrentHP(mcr)).."", 16, WorldToScreen(0, GetOrigin(mcr)).x-15, WorldToScreen(0, GetOrigin(mcr)).y-55, ARGB(255,33,184,184))
+					elseif GetObjectName(minion) == "SRU_OrderMinionRanged" then
+						mcr = minion
+						DrawText(""..math.ceil(GetCurrentHP(mcr)).."", 16, WorldToScreen(0, GetOrigin(mcr)).x-15, WorldToScreen(0, GetOrigin(mcr)).y-55, ARGB(255,227,52,75))
+					end
+				end
+				if HpMenu.sel.select.mcm:Value() then
+					if GetObjectName(minion) == "SRU_ChaosMinionMelee" then
+						mcm = minion
+						DrawText(""..math.ceil(GetCurrentHP(mcm)).."", 16, WorldToScreen(0, GetOrigin(mcm)).x-15, WorldToScreen(0, GetOrigin(mcm)).y-55, ARGB(255,33,184,184))
+					elseif GetObjectName(minion) == "SRU_OrderMinionMelee" then
+						mcm = minion
+						DrawText(""..math.ceil(GetCurrentHP(mcm)).."", 16, WorldToScreen(0, GetOrigin(mcm)).x-15, WorldToScreen(0, GetOrigin(mcm)).y-55, ARGB(255,227,52,75))
+					end
+				end
+				if HpMenu.sel.select.mcs:Value() then
+					if GetObjectName(minion) == "SRU_ChaosMinionSiege" then
+						mcs = minion
+						DrawText(""..math.ceil(GetCurrentHP(mcs)).."", 16, WorldToScreen(0, GetOrigin(mcs)).x-15, WorldToScreen(0, GetOrigin(mcs)).y-55, ARGB(255,33,184,184))
+					elseif GetObjectName(minion) == "SRU_OrderMinionSiege" then
+						mcs = minion
+						DrawText(""..math.ceil(GetCurrentHP(mcs)).."", 16, WorldToScreen(0, GetOrigin(mcs)).x-15, WorldToScreen(0, GetOrigin(mcs)).y-55, ARGB(255,227,52,75))
+					end
+				end
+			end
+		end
+	end
+end)
+
+PrintChat(string.format("<font color=\"#85EDD7\"><b>Welcome " ..GetUser().. " to Required's Minion HP Drawings .</b></font>"))
