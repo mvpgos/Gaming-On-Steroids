8dd31d41ec72d34b77a5fd665857aadb97f6c702
RequiredGoS <desafiatumenteyt@gmail.com>
2016-05-02 10:24:44 +0200

    Update MinionHP.lua

--- MinionHP/MinionHP.lua
+++ MinionHP/MinionHP.lua
@@ -1,10 +1,10 @@
-local ver = "0.12"
+local ver = "0.13"
 
 
 function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         PrintChat("New Version Found " .. data)
-        PrintChat("Downloading update, please wait...")
+        PrintChat("Downloading new Minion HP, please wait...")
         DownloadFileAsync("https://raw.githubusercontent.com/RequiredGoS/Gaming-On-Steroids/master/MinionHP/MinionHP.lua", SCRIPT_PATH .. "MinionHP.lua", function() PrintChat(string.format("<font color=\"#FC5743\"><b>Script Downloaded succesfully. please 2x f6</b></font>")) return end)
     end
 end
@@ -28,7 +28,7 @@ OnDraw(function()
                 if GetObjectName(minion):lower():find("melee") and not HpMenu.sel.select.mcm:Value() then return end
                 if GetObjectName(minion):lower():find("siege") and not HpMenu.sel.select.mcs:Value() then return end
                	if GetObjectName(minion):lower():find("order") then color = ARGB(255,227,52,75) else color = ARGB(255,33,184,184) end
-                DrawText(""..math.ceil(GetCurrentHP(minion)).."", 16, WorldToScreen(0, GetOrigin(minion)).x-15, WorldToScreen(0, GetOrigin(minion)).y-55, color)
+                DrawText(""..math.ceil(GetCurrentHP(minion)).."", 16, minion.pos2D.x-15, minion.pos2D.y-55, color)
             end
         end
     end
